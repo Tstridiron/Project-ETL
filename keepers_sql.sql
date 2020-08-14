fc8382cb786da87d54686b27b186514b33d9c6ed
Hung Chen <chen_chc@hotmail.com>
2020-08-13 21:34:33 -0400

    final commit

---
+++ keepers_sql.sql
@@ -0,0 +1,30 @@
+create table keepers_records (
+	index int primary key,
+	player_name varchar(50),
+	country varchar(3),
+	not_conceded dec,
+	conceded dec,
+	pksfaced int,
+	saves varchar(10),
+	height dec,
+	weight dec,
+	date timestamp,
+	overall_rating dec,
+	potential dec,
+	diving dec,
+	handling dec,
+	kicking dec,
+	positioning dec,
+	reflexes dec,
+	title varchar(30)	
+);
+
+
+
+
+
+
+
+
+
+
